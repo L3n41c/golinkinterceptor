dea0eb43d3dd894191a8fde40b54fbea7176fbf8
Lénaïc Huard <lenaic.huard@datadoghq.com>
2025-04-30 15:08:55 +0200

    Split the monolithic `main` function

--- cmd/executor/main.go
+++ cmd/executor/main.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"database/sql"
 	"encoding/json"
+	"errors"
 	"flag"
 	"fmt"
 	"log"
@@ -18,8 +19,77 @@ import (
 
 const dbPath = "link.db"
 
-//nolint:gocyclo
 func main() {
+	ctx := context.Background()
+
+	config, err := parseConfig(ctx)
+	if err != nil {
+		log.Fatalf("Error: unable to parse config: %v", err)
+	}
+
+	// Open the database
+	db, err := sql.Open("sqlite3", "file:"+dbPath+"?mode=ro&_foreign_keys=true")
+	if err != nil {
+		log.Fatalf("Error: unable to open database %q: %v", dbPath, err)
+	}
+	defer db.Close()
+
+	tx, err := db.BeginTx(ctx, nil)
+	if err != nil {
+		log.Fatalf("Error: unable to begin transaction: %v", err) //nolint:gocritic
+	}
+	defer tx.Rollback() //nolint:errcheck
+
+	linkCommandID, mainPackage, err := getLinkCommandID(ctx, tx, config.binaryName, config.buildTags)
+	if err != nil {
+		log.Fatalf("Error: unable to get link command ID: %v", err)
+	}
+
+	importcfgFileName, err := getImportcfg(ctx, tx, linkCommandID)
+	if err != nil {
+		log.Fatalf("Error: unable to get importcfg: %v", err)
+	}
+
+	binaryFile, err := os.CreateTemp("", config.binaryName)
+	if err != nil {
+		log.Fatalf("Error: unable to create binary file: %v", err)
+	}
+
+	args, err := getLinkerCommandArgs(ctx, tx, linkCommandID, mainPackage, binaryFile.Name(), importcfgFileName)
+	if err != nil {
+		log.Fatalf("Error: unable to get link command args: %v", err)
+	}
+
+	// Invoke the linker
+	log.Printf("Link command: %s %s\n", config.linker, strings.Join(args, " "))
+	out, err := exec.CommandContext(ctx, config.linker, args...).Output() //nolint:gosec
+	log.Print(string(out))
+	if err != nil {
+		if err, ok := err.(*exec.ExitError); ok {
+			log.Print(string(err.Stderr))
+			os.Exit(err.ExitCode())
+		}
+		log.Fatalf("Error: linker command failed: %v", err)
+	}
+
+	if err := os.Remove(importcfgFileName); err != nil {
+		log.Fatalf("Error: unable to remove importcfg file: %v", err)
+	}
+
+	log.Printf("Exec: %s %s\n", binaryFile.Name(), config.args)
+	if err := syscall.Exec(binaryFile.Name(), config.args, os.Environ()); err != nil { //nolint:gosec
+		log.Fatalf("Error: exec failed: %v", err)
+	}
+}
+
+type Config struct {
+	linker     string
+	binaryName string
+	buildTags  []string
+	args       []string
+}
+
+func parseConfig(_ context.Context) (config Config, err error) {
 	link := flag.String("link", "", "File path to the linker executable (Should be \"$(go env GOTOOLDIR)/link\")")
 	tags := flag.String("tags", "", "Build tags to use")
 	flag.Parse()
@@ -29,33 +99,23 @@ func main() {
 		os.Exit(2)
 	}
 
-	binaryName := flag.Arg(0)
-	var buildTags []string
+	config.linker = *link
+	config.binaryName = flag.Arg(0)
+	config.args = flag.Args()[1:]
 	if *tags != "" {
-		buildTags = strings.Split(*tags, ",")
-		slices.Sort(buildTags)
+		config.buildTags = strings.Split(*tags, ",")
+		slices.Sort(config.buildTags)
 	}
+
+	return
+}
+
+func getLinkCommandID(ctx context.Context, tx *sql.Tx, binaryName string, buildTags []string) (linkCommandID int, mainPackage string, err error) {
 	buildTagsJSON, err := json.Marshal(buildTags)
 	if err != nil {
-		log.Fatalf("unable to marshal build tags: %v", err)
+		return 0, "", fmt.Errorf("unable to marshal build tags: %w", err)
 	}
 
-	ctx := context.Background()
-
-	// Open the database
-	db, err := sql.Open("sqlite3", "file:"+dbPath+"?mode=ro&_foreign_keys=true")
-	if err != nil {
-		log.Fatalf("unable to open database %q: %v", dbPath, err)
-	}
-	defer db.Close()
-
-	tx, err := db.BeginTx(ctx, nil)
-	if err != nil {
-		log.Fatalf("unable to begin transaction: %v", err) //nolint:gocritic
-	}
-	defer tx.Rollback() //nolint:errcheck
-
-	// Get the link command ID
 	row := tx.QueryRowContext(ctx, `
 SELECT link_command_id, package_file.file
 FROM link_command
@@ -63,17 +123,29 @@ NATURAL JOIN build_tags
 LEFT JOIN package_file ON link_command.main_package_id = package_file.package_file_id
 WHERE binary_name = ? AND tags = jsonb(?);`,
 		binaryName, buildTagsJSON)
-	var linkCommandID int
-	var mainPackage string
 	if err := row.Scan(&linkCommandID, &mainPackage); err != nil {
 		if err == sql.ErrNoRows {
 			fmt.Fprintf(os.Stderr, "No link command found for %q with build tags %q\n", binaryName, buildTags)
 			os.Exit(1)
 		}
-		log.Fatalf("unable to query link command ID: %v", err)
+		return 0, "", fmt.Errorf("unable to query link command ID: %w", err)
 	}
 
-	// Get the importcfg
+	return
+}
+
+func getImportcfg(ctx context.Context, tx *sql.Tx, linkCommandID int) (importcfgFileName string, err error) {
+	importcfgFile, err := os.CreateTemp("", "importcfg.link")
+	if err != nil {
+		return "", fmt.Errorf("unable to create importcfg file: %w", err)
+	}
+	defer func() {
+		if err2 := importcfgFile.Close(); err2 != nil {
+			err = errors.Join(err, fmt.Errorf("unable to close importcfg file: %w", err2))
+		}
+	}()
+	importcfgFileName = importcfgFile.Name()
+
 	rows, err := tx.QueryContext(ctx, `
 SELECT 'packagefile ' || package || '=' || file
 FROM package_file
@@ -85,62 +157,61 @@ FROM importcfg_additional_lines
 WHERE link_command_id = ?;`,
 		linkCommandID, linkCommandID)
 	if err != nil {
-		log.Fatalf("unable to query importcfg: %v", err)
+		return "", fmt.Errorf("unable to query importcfg: %w", err)
 	}
-	defer rows.Close()
+	defer func() {
+		if err2 := rows.Close(); err2 != nil {
+			err = errors.Join(err, fmt.Errorf("unable to close importcfg rows: %w", err2))
+		}
+	}()
 
-	importcfgFile, err := os.CreateTemp("", "importcfg.link")
-	if err != nil {
-		log.Fatalf("unable to create importcfg file: %v", err)
-	}
 	for rows.Next() {
 		var line string
 		if err := rows.Scan(&line); err != nil {
-			log.Fatalf("unable to scan importcfg line: %v", err)
+			return "", fmt.Errorf("unable to scan importcfg line: %w", err)
 		}
 		log.Printf("%s --- %s\n", importcfgFile.Name(), line)
 		if _, err := fmt.Fprintln(importcfgFile, line); err != nil {
-			log.Fatalf("unable to write importcfg line: %v", err)
+			return "", fmt.Errorf("unable to write importcfg line: %w", err)
 		}
 	}
 	if err := rows.Err(); err != nil {
-		log.Fatalf("error reading importcfg rows: %v", err)
-	}
-	if err := importcfgFile.Close(); err != nil {
-		log.Fatalf("unable to close importcfg file: %v", err)
+		return "", fmt.Errorf("error reading importcfg rows: %w", err)
 	}
 
-	// Get the linker command
-	binaryFile, err := os.CreateTemp("", binaryName)
-	if err != nil {
-		log.Fatalf("unable to create binary file: %v", err)
-	}
+	return
+}
 
-	// Get the link command arguments
-	rows, err = tx.QueryContext(ctx, `
+func getLinkerCommandArgs(ctx context.Context, tx *sql.Tx, linkCommandID int, mainPackage, binaryFileName, importcfgFileName string) (args []string, err error) {
+	rows, err := tx.QueryContext(ctx, `
 SELECT arg
 FROM link_command_args
 WHERE link_command_id = ?
 ORDER BY pos;`,
 		linkCommandID)
 	if err != nil {
-		log.Fatalf("unable to query link command args: %v", err)
+		return nil, fmt.Errorf("unable to query link command args: %w", err)
 	}
-	defer rows.Close()
-	args := []string{}
+	defer func() {
+		if err2 := rows.Close(); err2 != nil {
+			err = errors.Join(err, fmt.Errorf("unable to close link command args rows: %w", err2))
+
+		}
+	}()
+
 	var prevArg string
 	for rows.Next() {
 		var arg string
 		if err := rows.Scan(&arg); err != nil {
-			log.Fatalf("unable to scan link command arg: %v", err)
+			return nil, fmt.Errorf("unable to scan link command arg: %w", err)
 		}
 
 		if arg == "PLACEHOLDER" {
 			switch prevArg {
 			case "-o":
-				arg = binaryFile.Name()
+				arg = binaryFileName
 			case "-importcfg":
-				arg = importcfgFile.Name()
+				arg = importcfgFileName
 			}
 		}
 
@@ -152,27 +223,8 @@ ORDER BY pos;`,
 		prevArg = arg
 	}
 	if err := rows.Err(); err != nil {
-		log.Fatalf("error reading link command rows: %v", err)
+		return nil, fmt.Errorf("error reading link command rows: %w", err)
 	}
 
-	// Invoke the linker
-	log.Printf("Link command: %s %s\n", *link, strings.Join(args, " "))
-	out, err := exec.CommandContext(ctx, *link, args...).Output() //nolint:gosec
-	log.Print(string(out))
-	if err != nil {
-		if err, ok := err.(*exec.ExitError); ok {
-			log.Print(string(err.Stderr))
-			os.Exit(err.ExitCode())
-		}
-		log.Fatalf("linker command failed: %v", err)
-	}
-
-	if os.Remove(importcfgFile.Name()) != nil {
-		log.Fatalf("unable to remove importcfg file: %v", err)
-	}
-
-	log.Printf("Exec: %s %s\n", binaryFile.Name(), flag.Args()[1:])
-	if err := syscall.Exec(binaryFile.Name(), flag.Args()[1:], os.Environ()); err != nil { //nolint:gosec
-		log.Fatalf("exec failed: %v", err)
-	}
+	return
 }
